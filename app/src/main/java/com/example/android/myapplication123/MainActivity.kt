4bdbd1e99a5fa4a36c0e3f6fb33121333cc778fa
Felipe Cook <felipepcook@gmail.com>
2022-07-18 21:00:52 +0800

    Checking out the new view binding feature

--- app/src/main/java/com/example/android/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/android/myapplication123/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android.myapplication123
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.android.myapplication123.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.bind(findViewById(R.id.root))
+
+        val rollButton: Button = binding.rollButton
     }
 }
