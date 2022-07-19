84627ab36295faeed8f5126651a6c87e346da408
Felipe Cook <felipepcook@gmail.com>
2022-07-19 20:14:14 +0800

    When rollButton clicked, generates random number into resultText view

--- app/src/main/java/com/example/android/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/android/myapplication123/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.android.myapplication123
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.android.myapplication123.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +21,14 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "lets roll"
 
         rollButton.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = binding.resultText
+
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
