4ad3dc10f1dd57ccdf20690920e4308a308d9123
Felipe Cook <felipepcook@gmail.com>
2022-07-20 20:11:17 +0800

    Updated diceView to be a field instead of local var and learned about lateinit modifier

--- app/src/main/java/com/example/android/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/android/myapplication123/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.myapplication123
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.android.myapplication123.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +25,24 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+        diceImage= binding.diceImage
     }
 
     private fun rollDice() {
-        val resultText: TextView = binding.resultText
 
-        val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+
+
     }
 }
